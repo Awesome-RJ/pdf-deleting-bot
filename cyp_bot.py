55607b37a6fd5f0573193b2f5fc647bea529764d
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2022-11-22 14:50:02 +0530

    Update cyp_bot.py

--- cyp_bot.py
+++ cyp_bot.py
@@ -1,8 +1,8 @@
 import os
 import asyncio
+from os import getenv
 
 from pyrogram import filters, Client
-from os import getenv
 
 api_id = int(getenv("API_ID"))
 api_hash = getenv("API_HASH")
@@ -13,13 +13,17 @@ pgram = Client(
     name=PyroGram,
     api_id=api_id,
     api_hash=api_hash,
-    bot_token=bot_token
+    bot_token=bot_token,
+#    workers=min(32, os.cpu_count() + 4),
+#    sleep_threshold=60,
+#    in_memory=True,
 )
 print("Bot starting")
 
-@pgram.on_message(filters.photo & filters.video & filters.document, group=2)
+@pgram.on_message(filters.document, group=2)
 async def media_files(client, message) -> None:
     await asyncio.sleep(1)
     await message.delete()
+    await bot.send_message(message.chat.id, "Alert!\nmedia detected!")
 
 pgram.run()
